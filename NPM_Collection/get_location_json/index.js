5bf16b65b71df20df02ef59d721aac4836e6223d
WIssn <1726025894@qq.com>
2023-03-14 11:18:52 +0800

    ss

--- NPM_Collection/get_location_json/index.js
+++ NPM_Collection/get_location_json/index.js
@@ -3,7 +3,7 @@
  * @param {{method: String, path: String, isAsync: Boolean }} config 
  * @returns JSON String
  */
-function getLocaleJson({ method, path, isAsync }) {
+function getLocaleJson(method, path, isAsync) {
     var request = new XMLHttpRequest()
     request.open(method, path, isAsync)
     request.send(null)
